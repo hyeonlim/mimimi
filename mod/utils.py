1cd431e400daf67e6e473ef5cf2ed02acbd646ae
hyeonlim <guswjd001024@gmailcom>
2023-09-22 18:05:54 +0900

    first commit

---
+++ mod/utils.py
@@ -0,0 +1,33 @@
+import math
+import random
+
+def sq_res(x):
+    return math.sqrt(x)
+    
+def mt_sinpi():
+    return math.sin(math.pi /2)
+
+def mt_elog():
+     return math.log(math.e)
+
+def mt_exp(x):
+    return math.exp(x)
+
+def mt_pi():
+    return math.pi
+
+# ============================================
+
+def rd_int(x, y):
+    return random.randint(x, y)
+
+def rd_list(this):
+    return random.choice(this)
+
+def rd_rd():
+    return random.random()
+
+def rd_nvres():
+    return random.normalvariate(0, 1)
+    
+    
